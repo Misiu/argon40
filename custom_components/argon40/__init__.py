d0ade0b4a8364670afdb951630d32da416b98ebc
Tomasz <t.jagusz@gmail.com>
2024-11-08 11:25:31 +0100

    Update __init__.py
    pi5 support

--- custom_components/argon40/__init__.py
+++ custom_components/argon40/__init__.py
@@ -2,7 +2,6 @@
 import logging
 from typing import Any
 
-from RPi import GPIO  # pylint: disable=import-error
 from custom_components.argon40.const import (
     ATTR_ALWAYS_ON_NAME,
     ATTR_SPEED_NAME,
@@ -12,10 +11,10 @@ from custom_components.argon40.const import (
     STARTUP_MESSAGE,
 )
 from homeassistant.const import EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
-from homeassistant.core import callback
+from homeassistant.core import callback, HomeAssistant
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.typing import ConfigType, HomeAssistantType, ServiceDataType
-from smbus import SMBus
+from homeassistant.helpers.typing import ConfigType, ServiceDataType
+from smbus2 import SMBus
 import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
@@ -24,67 +23,41 @@ SERVICE_SET_FAN_SPEED_SCHEMA = vol.Schema(
     {vol.Required(ATTR_SPEED_NAME): vol.All(vol.Coerce(int), vol.Range(min=0, max=100))}
 )
 
-SERVICE_SET_MODE_SCHEMA = vol.Schema({vol.Required(ATTR_ALWAYS_ON_NAME): cv.boolean})
+#I2C addresses
+ARGONONE_FAN_ADDRESS=0x1a
 
-I2C_ADDRESS = 0x1A
+#ArgonOne Reg Addresses
+ADDR_ARGONONEREG_DUTYCYCLE=0x80
 
-
-async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Argon40 component."""
 
     _LOGGER.info(STARTUP_MESSAGE)
 
     try:
-        rev = GPIO.RPI_REVISION
-        if rev == 2 or rev == 3:
-            bus = SMBus(1)
+        bus = SMBus(1)
+        
+        bus.write_byte_data(ARGONONE_FAN_ADDRESS, ADDR_ARGONONEREG_DUTYCYCLE, 10)
+
+        # Run check support function on startup
+        if argonregister_checksupport(bus):
+            _LOGGER.info("Argon case detected and supported.")
         else:
-            bus = SMBus(0)
-
-        # @callback
-        # def cleanup_gpio(event: Any) -> None:
-        #     """Stuff to do before stopping."""
-        #     GPIO.cleanup()
-
-        # # not sure if @callback needed
-        # @callback
-        # def prepare_gpio(event: Any) -> None:
-        #     """Stuff to do when Home Assistant starts."""
-        #     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, cleanup_gpio)
-
-        # hass.bus.listen_once(EVENT_HOMEASSISTANT_START, prepare_gpio)
-
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        shutdown_pin = 4
-        GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-        @callback
-        def event_detect_callback(channel):
-            if GPIO.input(shutdown_pin):
-                _LOGGER.debug("Rising edge detected on 4")
-                # hass.bus.async_fire("argon40_event", {"key": "pressed"})
-            else:
-                _LOGGER.debug("Falling edge detected on 4")
-                hass.bus.async_fire("argon40_event", {"action": "double-tap"})
-
-        GPIO.add_event_detect(shutdown_pin, GPIO.BOTH, callback=event_detect_callback)
-
-        bus.write_byte(I2C_ADDRESS, 10)
+            _LOGGER.error("Argon case not detected or not supported.")
 
     except IOError as err:
         _LOGGER.exception(
             "Error %d, %s accessing 0x%02X: Check your I2C address",
             err.errno,
             err.strerror,
-            I2C_ADDRESS,
+            ARGONONE_FAN_ADDRESS,
         )
         return False
 
     async def set_fan_speed(service: ServiceDataType) -> None:
         value = service.data.get(ATTR_SPEED_NAME)
-        _LOGGER.debug("Set fan speed to %s", value)
-        bus.write_byte(I2C_ADDRESS, value)
+        _LOGGER.error("Set fan speed to %s", value)
+        bus.write_byte_data(ARGONONE_FAN_ADDRESS, ADDR_ARGONONEREG_DUTYCYCLE, value)
         hass.bus.async_fire("argon40_event", {"speed": value})
 
     hass.services.async_register(
@@ -94,18 +67,37 @@ async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
         schema=SERVICE_SET_FAN_SPEED_SCHEMA,
     )
 
-    async def set_mode(service: ServiceDataType) -> None:
-        value = service.data.get(ATTR_ALWAYS_ON_NAME)
-        _LOGGER.debug("Set always on mode to %s", value)
-        if value:
-            bus.write_byte(I2C_ADDRESS, 254)  # 0xfe - always on mode
-        else:
-            bus.write_byte(I2C_ADDRESS, 253)  # 0xfd - the default mode
-
-        hass.bus.async_fire("argon40_event", {"always_on": value})
-
-    hass.services.async_register(
-        DOMAIN, SERVICE_SET_MODE, set_mode, schema=SERVICE_SET_MODE_SCHEMA,
-    )
-
     return True
+    
+def argonregister_checksupport(bus: SMBus) -> bool:
+    """Check if Argon case is supported."""
+    if bus is None:
+        return False
+    try:
+        oldval = argonregister_getbyte(bus, ADDR_ARGONONEREG_DUTYCYCLE)
+        _LOGGER.debug("old value %s", oldval)
+        newval = oldval + 1
+        if newval >= 100:
+            newval = 98
+        argonregister_setbyte(bus, ADDR_ARGONONEREG_DUTYCYCLE, newval)
+        read_back_val = argonregister_getbyte(bus, ADDR_ARGONONEREG_DUTYCYCLE)
+        
+        # Restore original value
+        argonregister_setbyte(bus, ADDR_ARGONONEREG_DUTYCYCLE, oldval)
+        
+        return read_back_val != oldval
+    except Exception as e:
+        _LOGGER.error("Error checking Argon support: %s", e)
+        return False
+        
+def argonregister_getbyte(bus, address: int) -> int:
+    """Read a single byte from a specific register address."""
+    if bus is None:
+        return 0
+    return bus.read_byte_data(ARGONONE_FAN_ADDRESS, address)
+
+def argonregister_setbyte(bus, address: int, bytevalue: int) -> None:
+    """Write a single byte to a specific register address, with a delay for stability."""
+    if bus is None:
+        return
+    bus.write_byte_data(ARGONONE_FAN_ADDRESS, address, bytevalue)
